6298da3c6b7c21ea29e8834185267f5a408f4c1e
Chris Beddome <chrisbeddome@gmail.com>
2022-07-04 12:22:14 -0400

    fonts

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,14 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
       </Head>
