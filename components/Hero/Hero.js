648a75630560571535a409b7989e72e8de11e114
Chris Beddome <chrisbeddome@gmail.com>
2022-07-06 10:53:40 -0400

    work on hero

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -5,51 +5,42 @@ export default function Hero() {
   return (
     <div className={styles.hero}>
       <div className={styles["hero-left"]}>
-        <Image
-          src="/images/clay-with-tass.png"
-          priority
-          height={132}
-          width={596}
-        />
-        <button>BUY OUR STUFF</button>
+        <div className={styles["hero-img"]}>
+          <Image
+            src={`/images/spacestuff.jpg`}
+            priority
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
       </div>
       <div className={styles["hero-right"]}>
         <div>
           <Image
-            className={styles["hero-image"]}
             src="/images/neat-things.jpg"
             priority
             layout="fill"
             objectFit="cover"
           />
         </div>
-        <div>
-          <Image
-            className={styles["hero-image"]}
-            src="/images/products/3.jpg"
-            priority
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-        <div>
-          <Image
-            className={styles["hero-image"]}
-            src="/images/products/2.jpg"
-            priority
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-        <div>
-          <Image
-            className={styles["hero-image"]}
-            src="/images/products/1.jpg"
-            priority
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
+        {[1, 2, 3, 8, 5, 6, 7, 4].map((num) => {
+          return (
+            <div>
+              <Image
+                className={styles["hero-image"]}
+                src={`/images/products/${num}.jpg`}
+                priority
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles["hero-main"]}>
+        <div className={styles.blurry}></div>
+        <h1>Animatrium Studio</h1>
+        <button>Explore</button>
       </div>
     </div>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
           rel="stylesheet"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&family=Bebas+Neue&display=swap"
           rel="stylesheet"
         />
       </Head>
