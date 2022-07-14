1d125f49c2180d30def0fad365b2f03a16c04ed3
Chris Beddome <chrisbeddome@gmail.com>
2022-07-13 19:46:08 -0500

    remove preconnect

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
           rel="stylesheet"
