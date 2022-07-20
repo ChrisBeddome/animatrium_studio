58cdc44c3e2775afed53781f70d373d6df329c7a
Chris Beddome <chrisbeddome@gmail.com>
2022-07-20 16:17:42 -0400

    added basic logging, fixed <title> tag errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import "/styles/reset.scss";
 import "/styles/global.scss";
 import "/styles/spacing.scss";
+import Head from "next/head";
 
 import DefaultLayout from "/components/layouts/Default";
 
 export default function App({ Component, pageProps }) {
-const Layout = (Component.getLayout && Component.getLayout()) || DefaultLayout
+  const Layout =
+    (Component.getLayout && Component.getLayout()) || DefaultLayout;
   return (
-    <Layout>
-      <Component {...pageProps}></Component>
-    </Layout>
+    <>
+      <Head>
+        <title>Animatrium Studio</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps}></Component>
+      </Layout>
+    </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
           rel="stylesheet"
         />
-        <title>Animatrium Studio</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
       <body>
