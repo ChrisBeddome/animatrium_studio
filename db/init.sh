00999593c5653773da0f1c5a96153a703b114a90
Chris Beddome <chrisbeddome@gmail.com>
2022-11-12 12:48:34 -0500

    DRY up  db.init

--- db/init.sh
+++ db/init.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-mysql -u root -p$MARIADB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -u root -p$MARIADB_ROOT_PASSWORD -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
+run_sql () {
+  mysql -u root -p$MARIADB_ROOT_PASSWORD -e $1
+}
+
+run_sql "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+run_sql "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
 
