f40da0fda5bba05554740c9ca9e5126636f5aad5
Chris Beddome <chrisbeddome@gmail.com>
2022-11-13 21:25:51 -0500

    remove error handling, must rethink

--- api/src/app.js
+++ api/src/app.js
@@ -1,15 +1,11 @@
 import express from "express"
 import throwMissingVarError from "../lib/utils/throwMissingVarError.js"
-import { generateNotFoundError, logError, sendErrorResponse } from "./middleware/errorHandler.js"
 import entryPoint from "./routes/entryPoint.js"
 
 const app = express()
 const port = parseInt(process.env.API_PORT) || (() => { throwMissingVarError("API_PORT") })()
 
 app.use("/", entryPoint);
-app.use(generateNotFoundError); 
-app.use(logError);
-app.use(sendErrorResponse);
 
 app.listen(port, () => {
   console.log(`Server started. Listening for requests on port ${port}...`)
--- api/src/controllers/products.js
+++ api/src/controllers/products.js
@@ -2,13 +2,18 @@ import Product from "../models/product.js"
 
 const getAllProducts = async (_req, res, _next) => {
   const products = await Product.findAll()
-  res.send({products: products})
+  res.send(products)
 };
 
 const getProduct = async (req, res, _next) =>   {
   const productID = req.params.productID
-  const product = await Product.findOne({ where: { id: productID } })
-  res.send({product: product})
+  const product = await Product.findByPk(productID)
+
+  if (product) {
+    res.send(product)
+  } else {
+    res.send({})
+  }
 }
 
 export default {
