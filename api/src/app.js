ac9ed68e8b6338729e6dbed8d2b5082014dae921
Chris Beddome <chrisbeddome@gmail.com>
2022-12-10 14:14:48 -0500

    cluster api

--- api/src/app.js
+++ api/src/app.js
@@ -1,13 +1,45 @@
+import cluster from 'node:cluster'
+import { cpus } from 'node:os'
 import express from "express"
 import throwMissingVarError from "../lib/utils/throwMissingVarError.js"
 import entryPoint from "./routes/entryPoint.js"
 
-const app = express()
 const port = parseInt(process.env.API_PORT) || (() => { throwMissingVarError("API_PORT") })()
 
-app.use("/", entryPoint);
+if (cluster.isMaster) {
+  forkWorkers(getWorkerCount())
+} else {
+  startExpress(port)
+}
 
-app.listen(port, () => {
-  console.log(`Server started. Listening for requests on port ${port}...`)
-})
+function getWorkerCount() {
+  const cpuCount = cpus().length
+  const maxWorkerCount = parseInt(process.env.API_MAX_WORKER_COUNT) || cpuCount
+  return Math.min(maxWorkerCount, cpuCount)
+}
 
+function forkWorkers(workerCount) {
+  console.log(`Master ${process.pid} is running`)
+  console.log(`Forking ${workerCount} worker(s)`)
+
+  for (let i = 0; i < workerCount; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`)
+    console.log("Forking another worker")
+    cluster.fork()
+  })
+}
+
+function startExpress(port) {
+  console.log(`Worker ${process.pid} started`)
+  const app = express()
+
+  app.use("/", entryPoint)
+
+  app.listen(port, () => {
+    console.log(`Worker ${process.pid} - Server started. Listening for requests on port ${port}...`)
+  })
+}
