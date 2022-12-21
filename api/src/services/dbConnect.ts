512a319b154026301255d7de230e8eeaa9d3639c
Chris Beddome <chrisbeddome@gmail.com>
2022-12-21 16:37:36 -0500

    setup db connection via knex

--- api/src/services/dbConnect.ts
+++ api/src/services/dbConnect.ts
@@ -1,9 +1,23 @@
 import requireEnvVar from 'lib/utils/requireEnvVar'
+import knex from 'knex'
 
-const hostname: string = requireEnvVar('DB_HOST')
+const host: string = requireEnvVar('DB_HOST')
 const port: number = Number(requireEnvVar('DB_PORT'))
 const user: string = requireEnvVar('DB_USER')
 const password: string = requireEnvVar('DB_PASSWORD')
 const database: string = requireEnvVar('DB_NAME')
 
-export default "TODO"
+const db = knex({
+  client: 'mariadb',
+  connection: {
+    host,
+    port,
+    user,
+    password,
+    database,
+  },
+  pool: {min: 0, max: 10},
+  acquireConnectionTimeout: 10000
+})
+
+export default db
