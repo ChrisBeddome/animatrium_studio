6112bb4819e75d54ae560470fbf4a655397f1887
Chris Beddome <chrisbeddome@gmail.com>
2023-09-21 19:21:35 -0400

    schema generation working

--- migration_service/bin/generateSchemaMigration.js
+++ migration_service/bin/generateSchemaMigration.js
@@ -1,33 +1,34 @@
 import fs from 'fs'
 import path from 'path'
+import url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 // YYYYMMDDHHMMSSsssZ ie 20230921224331227Z
 const getDateString = () => (new Date()).toISOString().replace(/[-T:.]/g, '')
 
-const generateFileName = userSuppliedName => `${getDateString()}_${userSuppliedName}`
+const generateFileName = userSuppliedName => `${getDateString()}_${userSuppliedName}.js`
 
 const createFile = userInput => {
-	const filepath = path.join('../migrations/schema/', generateFileName(userSuppliedName))
-	const content = `
-		const up = () => {
-			// return sql here
-		}
+	const filepath = path.join(__dirname, '../migrations/schema/', generateFileName(userInput))
+	const content = `const up = () => {
+	// return sql here
+}
 
-		const down = () => {
-			// return sql here
-		}
-	`
+const down = () => {
+	// return sql here
+}
+`
 
-	fs.writeFile(filename, textToWrite, err => {
+	fs.writeFile(filepath, content, err => {
 		if (err) {
 			console.error(`Error creating file: ${err}`)
 		} else {
-			console.log(`File "${filename}" created`)
+			console.log(`File "${filepath}" created`)
 		}
 	})
 }
 
-const userInput = process.argv[0]
+const userInput = process.argv[2]
 createFile(userInput)
 
 
