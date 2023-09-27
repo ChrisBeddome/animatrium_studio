d6e93c407564d429bd746053a606e7da30a1e3e6
Chris Beddome <chrisbeddome@gmail.com>
2023-09-27 19:29:53 -0400

    refactor

--- migration_service/bin/generateSchemaMigration.js
+++ migration_service/bin/generateSchemaMigration.js
@@ -1,31 +1,40 @@
-import fs from 'fs'
+import fs from 'fs/promises'
 import path from 'path'
-import url from 'url';
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+import url from 'url'
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+const migrationDir = path.join(__dirname, '../migrations/schema/')
 
-// YYYYMMDDHHMMSSsssZ ie 20230921224331227Z
-const getDateString = () => (new Date()).toISOString().replace(/[-T:.]/g, '')
+const getDateString = () => new Date().toISOString().replace(/[-T:.]/g, '')
 
-const generateFileName = userSuppliedName => `${getDateString()}_${userSuppliedName}.js`
+const generateFileName = (userSuppliedName) => `${getDateString()}_${userSuppliedName}.js`
 
-const createFile = userInput => {
-  const filepath = path.join(__dirname, '../migrations/schema/', generateFileName(userInput))
-  const content = `export const up = () => {
-  // return sql here
+const generateFileContent = () => {
+  return `export const up = () => {
+  // return SQL query for migration up
 }
 
 export const down = () => {
-  // return sql here
+  // return SQL query for migration down
 }
 `
+}
 
-  fs.writeFile(filepath, content, err => {
-    if (err) {
-      console.error(`Error creating file: ${err}`)
-    } else {
-      console.log(`File "${filepath}" created`)
-    }
-  })
+const createFile = async (userInput) => {
+  if (!userInput || userInput.trim() === '') {
+    console.error('Error: Please provide a valid file name.')
+    return
+  }
+
+  const fileName = generateFileName(userInput)
+  const filePath = path.join(migrationDir, fileName)
+  const fileContent = generateFileContent()
+
+  try {
+    await fs.writeFile(filePath, fileContent)
+    console.log(`File "${fileName}" created at "${filePath}"`)
+  } catch (error) {
+    console.error(`Error creating file: ${error.message}`)
+  }
 }
 
 const userInput = process.argv[2]
