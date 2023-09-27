51b5a122807d61d1df9ee830ffe555261ce89b20
Chris Beddome <chrisbeddome@gmail.com>
2023-09-26 22:19:26 -0400

    tabs > spaces

--- migration_service/bin/generateSchemaMigration.js
+++ migration_service/bin/generateSchemaMigration.js
@@ -9,23 +9,23 @@ const getDateString = () => (new Date()).toISOString().replace(/[-T:.]/g, '')
 const generateFileName = userSuppliedName => `${getDateString()}_${userSuppliedName}.js`
 
 const createFile = userInput => {
-	const filepath = path.join(__dirname, '../migrations/schema/', generateFileName(userInput))
-	const content = `export const up = () => {
-	// return sql here
+  const filepath = path.join(__dirname, '../migrations/schema/', generateFileName(userInput))
+  const content = `export const up = () => {
+  // return sql here
 }
 
 export const down = () => {
-	// return sql here
+  // return sql here
 }
 `
 
-	fs.writeFile(filepath, content, err => {
-		if (err) {
-			console.error(`Error creating file: ${err}`)
-		} else {
-			console.log(`File "${filepath}" created`)
-		}
-	})
+  fs.writeFile(filepath, content, err => {
+    if (err) {
+      console.error(`Error creating file: ${err}`)
+    } else {
+      console.log(`File "${filepath}" created`)
+    }
+  })
 }
 
 const userInput = process.argv[2]
--- migration_service/bin/migrateSchema.js
+++ migration_service/bin/migrateSchema.js
@@ -7,62 +7,62 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const readdir = promisify(fs.readdir)
 
 const setupDir = directoryPath => {
-	if (!fs.existsSync(directoryPath)) {
-		fs.mkdirSync(directoryPath, { recursive: true })
-	} 
+  if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath, { recursive: true })
+  } 
 }
 
 const getTableSetupQuery = () => (`
-	CREATE TABLE IF NOT EXISTS schema_migrations (
-		name VARCHAR(255) NOT NULL,
-		PRIMARY KEY (name)
-	)
+  CREATE TABLE IF NOT EXISTS schema_migrations (
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (name)
+  )
 `)
 
 const getCompletedMigrationsQuery = () => (`
-	SELECT name FROM schema_migrations;
+  SELECT name FROM schema_migrations;
 `)
 
 const insertMigrationQuery = filename => (`
-	INSERT INTO schema_migrations (name) VALUES ('${filename}')
+  INSERT INTO schema_migrations (name) VALUES ('${filename}')
 `)
 
 const getFilesForMigration = connection => {
-	return new Promise(async (res, rej) => {
-		const completedMigrationFileNames = (await connection.execute(getCompletedMigrationsQuery())).map(row => row.name)
-		const allMigrationFileNames = await readdir(path.join(__dirname, '../migrations/schema/'))
-		res(allMigrationFileNames.filter(filename => !completedMigrationFileNames.includes(filename)))
-	})
+  return new Promise(async (res, rej) => {
+    const completedMigrationFileNames = (await connection.execute(getCompletedMigrationsQuery())).map(row => row.name)
+    const allMigrationFileNames = await readdir(path.join(__dirname, '../migrations/schema/'))
+    res(allMigrationFileNames.filter(filename => !completedMigrationFileNames.includes(filename)))
+  })
 }
 
 const migrateOne = (connection, filename) => {
-	return new Promise(async (res, rej) => {
-		try {
-			const module = await import(path.join(__dirname, `../migrations/schema/${filename}`));
-			await connection.execute(module.up())
-			await connection.execute(insertMigrationQuery(filename))
-			res()
-		} catch (e) {
-			rej(e)
-		}
-	})
+  return new Promise(async (res, rej) => {
+    try {
+      const module = await import(path.join(__dirname, `../migrations/schema/${filename}`));
+      await connection.execute(module.up())
+      await connection.execute(insertMigrationQuery(filename))
+      res()
+    } catch (e) {
+      rej(e)
+    }
+  })
 } 
 
 const migrateSchema = connection => {
-	return new Promise(async (res, rej) => {
-		setupDir(path.join(__dirname, `../migrations/schema/`))
-		await connection.execute(getTableSetupQuery())
+  return new Promise(async (res, rej) => {
+    setupDir(path.join(__dirname, `../migrations/schema/`))
+    await connection.execute(getTableSetupQuery())
     const filesToMigrate = await getFilesForMigration(connection)
-		try {
-			for (let i = 0; i < filesToMigrate.length; i++) {
-				await migrateOne(connection, filesToMigrate[i])
-			}
-		} catch(e) {
-			rej(e)
-		}
+    try {
+      for (let i = 0; i < filesToMigrate.length; i++) {
+        await migrateOne(connection, filesToMigrate[i])
+      }
+    } catch(e) {
+      rej(e)
+    }
 
-		res()
-	})
+    res()
+  })
 }
 
 transaction(migrateSchema)
