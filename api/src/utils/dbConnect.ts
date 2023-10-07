3aec6a133c1c4ba9ac22fdfb231c977aa6e7968e
Chris Beddome <chrisbeddome@gmail.com>
2023-10-07 04:01:19 -0400

    more robust dbConnect

--- api/src/utils/dbConnect.ts
+++ api/src/utils/dbConnect.ts
@@ -9,7 +9,39 @@ const dbConfig: mariadb.PoolConfig = {
   database: requireEnvVar('DB_NAME')
 }
 
-const	pool: mariadb.Pool = mariadb.createPool(dbConfig)
+let pool: mariadb.Pool | undefined
 
-export default pool
+const getPool = (): mariadb.Pool => {
+  pool = pool || mariadb.createPool(dbConfig)
+  return pool
+}
+
+const connect = async (): Promise<mariadb.PoolConnection> => {
+  return await getPool().getConnection()
+}
+
+const query = async(sql: string): Promise<any> => {
+  const connection = await connect()
+  const result: any = await connection.execute(sql)
+  connection.release()
+  return result
+}
+
+type transactionCallback = (connection: mariadb.PoolConnection) => Promise<any>
+const transaction = async (fn: transactionCallback): Promise<any> => {
+  const connection: mariadb.PoolConnection = await connect()
+  try {
+    await connection.beginTransaction()
+    const val: any = await fn(connection)
+    await connection.commit()
+    return val
+  } catch (error) {
+    await connection.rollback()
+    throw error
+  } finally {
+    await connection.release()
+  }
+}
+
+export { query, transaction }
 
