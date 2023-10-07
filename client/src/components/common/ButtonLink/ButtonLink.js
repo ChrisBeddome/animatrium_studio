46ed53a41dc205439a0b426fb1a651c31a9e44c9
Chris Beddome <chrisbeddome@gmail.com>
2023-10-07 01:32:57 -0400

    fix hardcoded values for buttonlink

--- client/src/components/common/ButtonLink/ButtonLink.js
+++ client/src/components/common/ButtonLink/ButtonLink.js
@@ -5,8 +5,8 @@ import Link from "next/link"
 export default function ButtonLink({text, href, dark = false}) {
   return (
     <div className={`${styles["button-link"]} ${dark ? styles.dark : ''}`}>
-      <Link href="https://www.youtube.com/c/AnimatriumStudio">
-        <a>Check out our YouTube channel</a>
+      <Link href={href}>
+        <a>{text}</a>
       </Link>
     </div>
   )
