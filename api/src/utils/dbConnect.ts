43a234f4ae35beba6d781eb40913f96eb1847627
Chris Beddome <chrisbeddome@gmail.com>
2023-10-07 03:14:06 -0400

    types for dbConnect

--- api/src/utils/dbConnect.ts
+++ api/src/utils/dbConnect.ts
@@ -1,7 +1,7 @@
 import mariadb from 'mariadb'
 import requireEnvVar from '../../lib/utils/requireEnvVar.js'
 
-const dbConfig = {
+const dbConfig: mariadb.PoolConfig = {
   host: requireEnvVar('DB_HOST'),
   port: Number(requireEnvVar('DB_PORT')),
   user: requireEnvVar('DB_USER'),
@@ -9,7 +9,7 @@ const dbConfig = {
   database: requireEnvVar('DB_NAME')
 }
 
-const	pool = mariadb.createPool(dbConfig)
+const	pool: mariadb.Poolss = mariadb.createPool(dbConfig)
 
 export default pool
 
